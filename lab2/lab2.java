0926094faaf7e0afb16ff2e30ad3779303c805b3
SashaKononenko <alex327021@gmail.com>
2016-09-23 13:09:03 +0300

    2

---
+++ lab2/lab2.java
@@ -0,0 +1,12 @@
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*;
+
+class main {
+    public static void main(String[] args) {
+		JFrame window = new JFrame("Lab 2");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		JButton btn_create_object = new JButton("Create new o")
+	}
+}
