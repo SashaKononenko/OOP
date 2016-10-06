54dc7083c5750de737fd4a6e3fa150e3ca3045f4
SashaKononenko <alex327021@gmail.com>
2016-10-06 16:42:23 +0300

    03

--- lab2/lab2.java
+++ lab2/lab2.java
@@ -7,6 +7,42 @@ class main {
 		JFrame window = new JFrame("Lab 2");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
-		JButton btn_create_object = new JButton("Create new o")
+		landlinePhone phone = new landlinePhone();
+		
+		JTextField screen = new JTextField();
+		JButton greenTube = new JButton("green");
+		JButton redTube = new JButton("red");
+		JButton btnIncomingCall = new JButton("Test incoming call");
+		
+		window.getContentPane().add(screen,BorderLayout.NORTH);
+		window.getContentPane().add(greenTube,BorderLayout.WEST);
+		window.getContentPane().add(redTube,BorderLayout.EAST);
+		window.getContentPane().add(btnIncomingCall,BorderLayout.CENTER);
+		window.setSize(300,100);
+		greenTube.addActionListener(new ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					phone.SetScreen("saddasd");
+				}
+			}
+		);
+		
 	}
+}
+
+class landlinePhone {
+	boolean incomingCall;
+	boolean call;
+	boolean tubeOnPhone;
+	int number = 555214;
+	String screen = "";
+	
+	public void SetScreen(String str);
+	{
+		this.screen.setText(str);
+	}
+	
+	
+	
 }
