be857d11f4615331a40618261efb4cfa607b66b3
Unknown <alex327021@gmail.com>
2016-11-11 20:05:45 +0200

    11

--- lab5/lab5.java
+++ lab5/lab5.java
@@ -4,7 +4,9 @@ import java.awt.event.*;
 
 class main
 {
-	static dishes dis;
+
+	static dishes Dishes;
+	static cup Cup;
 	public static void main(String[] args)
 	{
 		JFrame window = new JFrame("Lab5");
@@ -29,40 +31,53 @@ class main
 		//Виведення елементів в вікно
 		window.getContentPane().add(constr);
 		window.getContentPane().add(create);
-		//window.getContentPane().add(set);
-		window.getContentPane().add(pnl);
 		window.getContentPane().add(showRes);
 		window.getContentPane().add(result);
+		window.getContentPane().add(set);
+		//window.getContentPane().add(pnl);
 		
 		window.setVisible(true);
 		window.setSize(500,300);
-		
-		
-		//cup Cup = new cup(12,2,4);
 		
 		create.addActionListener(new ActionListener()
 			{
 				public void actionPerformed(ActionEvent e)
 				{
+					if(Dishes != null)
+					{
+						window.getContentPane().remove(Dishes);
+						Dishes = null;
+					}
+					if(Cup != null)
+					{
+						window.getContentPane().remove(Cup);
+						Cup = null;
+
+					}
+
 					if ((String)constr.getSelectedItem() == "dishes(String a)")
 					{
-						dishes Dishes = new dishes("Glass");
-						Dishes.DrawWindow();
+						Dishes = new dishes("Glass");
+						window.getContentPane().repaint();
+						window.getContentPane().add(Dishes);
 					}
 					if ((String)constr.getSelectedItem() == "dishes(int a)")
 					{
-						dishes Dishes =  new dishes(143);
-						Dishes.DrawWindow();
+						Dishes =  new dishes(143);
+						window.getContentPane().repaint();
+						window.getContentPane().add(Dishes);
 					}
 					if ((String)constr.getSelectedItem() == "cup(int a, int b, int c)")
 					{
-						cup Cup =  new cup(23,12,43);
-						Cup.DrawWindow();
+						Cup =  new cup(23,12,43);
+						window.getContentPane().repaint();
+						window.getContentPane().add(Cup);
 					}
 					if ((String)constr.getSelectedItem() == "cup(int a, int b)")
 					{
-						cup Cup =  new cup(12,23);
-						Cup.DrawWindow();
+						Cup =  new cup(12,23);
+						window.getContentPane().repaint();
+						window.getContentPane().add(Cup);
 					}
 					result.setText("Create object");
 				}
@@ -72,9 +87,42 @@ class main
 			{
 				public void actionPerformed(ActionEvent e)
 				{
+					if(Dishes != null)
+					{
+						Dishes.DrawWindow();
+					}
+					if(Cup != null)
+					{
+						Cup.DrawWindow();
+					}
 				}
 			}						
 		);
+		showRes.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					
+					if(Dishes != null)
+					{
+						window.getContentPane().repaint();
+						window.getContentPane().add(Dishes);
+						result.setText("col:"+Dishes.col+"\n"
+									   +"material:"+Dishes.material+"\n"
+									   +"prise:"+Dishes.prise);
+					}
+					if(Cup != null)
+					{
+						window.getContentPane().repaint();
+						window.getContentPane().add(Cup);
+						result.setText("diameter:"+Cup.diameter+"\n"
+									   +"height:"+Cup.height+"\n"
+									   +"volume:"+Cup.volume);
+					}
+				}
+			}						
+		);
+
 	}
 }
 
@@ -98,12 +146,21 @@ class dishes extends JPanel
 		prise = a;
 	}
 	//Методи
+	public void paint (Graphics g) 
+	{
+		g.setColor(col);
+		g.drawRect(10,15,prise,50);
+		g.drawString(material, 50, 10);
+	}
+
 	void DrawWindow()
 	{
-		JFrame SetWin = new JFrame("Setting cup");
+		JFrame SetWin = new JFrame("Setting dishes");
 		GridLayout gbl = new GridLayout(4,1);
 		SetWin.setLayout(gbl);
-		
+		SetWin.setLocation(0,350);
+
+
 		JButton changeCol = new JButton("");
 		changeCol.setBackground(col);
 		col = Color.white;
@@ -120,15 +177,19 @@ class dishes extends JPanel
 		JLabel L_SetCol = new JLabel("Color:");
 		JLabel L_mat = new JLabel("Material:");
 		JLabel L_prs = new JLabel("Prise:");
-		
+		JButton apply = new JButton("Apply");
+
 		SetWin.getContentPane().add(L_SetCol);
 		SetWin.getContentPane().add(changeCol);
 		SetWin.getContentPane().add(L_mat);
 		SetWin.getContentPane().add(mat);
 		SetWin.getContentPane().add(L_prs);
 		SetWin.getContentPane().add(prs);
+		SetWin.getContentPane().add(apply);
+		
 		SetWin.setVisible(true);
 		SetWin.setSize(500,300);
+		
 		changeCol.addActionListener(new ActionListener() 
 			{
 				public void actionPerformed(ActionEvent e)
@@ -138,8 +199,21 @@ class dishes extends JPanel
 				}
 			}
 		);
-		
+		apply.addActionListener(new ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					material = (String)mat.getSelectedItem();
+					prise = Integer.parseInt(prs.getText());
+					
+				}
+			}
+		);
 	}
+	
+		
+	
+	
 }
 
 class cup extends dishes
@@ -147,6 +221,7 @@ class cup extends dishes
 	int diameter;
 	int height;
 	int volume;
+	
 	//Конструктори
 	cup(int a, int b, int c)
 	{
@@ -160,19 +235,25 @@ class cup extends dishes
 		height = b;
 		volume = a * b;
 	}
-	//Методи
+	@Override
+	public void paint (Graphics g) 
+		{
+			g.drawOval(20, diameter, height, volume);
+		}
 	void DrawWindow()
 	{
 		JFrame SetWin = new JFrame("Setting cup");
 		GridLayout gbl = new GridLayout(4,1);
 		SetWin.setLayout(gbl);
-		
+		SetWin.setLocation(0,350);
+
 		JTextField  diam = new JTextField();
 		JTextField  heig = new JTextField();
 		JTextField  vol = new JTextField();
 		JLabel L_diam = new JLabel("diameter:");
 		JLabel L_heig = new JLabel("height:");
 		JLabel L_vol = new JLabel("volume:");
+		JButton apply = new JButton("Apply");
 		
 		SetWin.getContentPane().add(L_diam);
 		SetWin.getContentPane().add(diam);
@@ -180,9 +261,23 @@ class cup extends dishes
 		SetWin.getContentPane().add(heig);
 		SetWin.getContentPane().add(L_vol);
 		SetWin.getContentPane().add(vol);
+		SetWin.getContentPane().add(apply);
 		SetWin.setVisible(true);
 		SetWin.setSize(500,300);
+		
+		apply.addActionListener(new ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					diameter = Integer.parseInt(diam.getText());
+					height = Integer.parseInt(heig.getText());
+					volume = Integer.parseInt(vol.getText());
+				}
+			}
+		);
 	}
+	
+	
 	
 	
 }
