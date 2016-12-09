84df7276422e4ad5c364a3272fbc529160c082d9
SashaKononenko <alex327021@gmail.com>
2016-12-09 17:06:52 +0200

    22

--- lab8/lab8.java
+++ lab8/lab8.java
@@ -115,9 +115,93 @@ class Telephone extends JPanel
 	
 }
 
-class Human
+class Human extends JPanel
 {
 	Telephone tel;
+	int x = 10;
+	int y = 10;
+	int height = 50;
+	int width = 50;
+
+	public void paint (Graphics g) 
+	{
+		g.drawOval(x, y, height, width);
+
+
+	}
+	
+	void DrawWindow()
+	{
+		JFrame SetWin = new JFrame("Setting Human");
+		GridLayout gbl = new GridLayout(5,2);
+		SetWin.setLayout(gbl);
+		SetWin.setLocation(0,350);
+
+		JTextField  _x = new JTextField();
+		JTextField  _y = new JTextField();
+		JTextField  _h = new JTextField();
+		JTextField  _w = new JTextField();
+		JLabel L_x = new JLabel("X:");
+		JLabel L_y = new JLabel("Y:");
+		JLabel L_height = new JLabel("Height:");
+		JLabel L_width = new JLabel("Width:");
+		JButton apply = new JButton("Input data");
+		JButton setTel = new JButton("Setting object Telephone");
+
+		SetWin.getContentPane().add(L_x);
+		SetWin.getContentPane().add(_x);
+		SetWin.getContentPane().add(L_y);
+		SetWin.getContentPane().add(_y);
+		SetWin.getContentPane().add(L_height);
+		SetWin.getContentPane().add(_h);
+		SetWin.getContentPane().add(L_width);
+		SetWin.getContentPane().add(_w);
+		SetWin.getContentPane().add(apply);
+		if (tel != null)
+		{
+			SetWin.getContentPane().add(setTel);
+		}
+		
+		SetWin.setVisible(true);
+		SetWin.setSize(600,300);
+		
+
+		apply.addActionListener(new ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					inputData(_x, _y, _h, _w);
+					
+				}
+			}
+		);
+		setTel.addActionListener(new ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					tel.DrawWindow();
+				}
+			}
+		);
+	}
+	void inputData(JTextField a, JTextField b, JTextField c, JTextField d)
+	{
+		x = Integer.parseInt(a.getText());
+		y = Integer.parseInt(b.getText());
+		height = Integer.parseInt(c.getText());
+		width = Integer.parseInt(d.getText());
+	}
+
+	String outputData()
+	{
+		String a = "x:"+x+"\n"+"y:"+y+"\n"+"height:"+height+"\n"+"width:"+width;
+		return a;
+	}
+
+	void addObject()
+	{
+		tel = new Telephone("Alcatel");
+	}
 }
 
 class WorkWithClassHuman
@@ -127,19 +211,22 @@ class WorkWithClassHuman
 		JFrame window = new JFrame("Lab8");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		GridLayout gbl = new GridLayout(2,0);
+		GridLayout gbl = new GridLayout(3,1);
 		window.setLayout(gbl);
 		
 
 		JTextArea result = new JTextArea();
 		JButton showRes = new JButton("Output data");
 		JButton set = new JButton("Setting");
-		//Виведення елементів в вікно
+		Human human = new Human();
+		JButton addTel = new JButton("add object");
 		
 		window.getContentPane().add(set);
 		window.getContentPane().add(showRes);
+		window.getContentPane().add(addTel);
 		window.getContentPane().add(result);
-		
+		window.getContentPane().add(human);
+
 		window.setVisible(true);
 		window.setSize(500,300);
 
@@ -148,7 +235,7 @@ class WorkWithClassHuman
 			{
 				public void actionPerformed(ActionEvent e)
 				{
-					
+					human.DrawWindow();
 				}
 			}
 						
@@ -157,11 +244,32 @@ class WorkWithClassHuman
 			{
 				public void actionPerformed(ActionEvent e)
 				{
+					window.getContentPane().remove(human);
+					if (human.tel != null)
+						window.getContentPane().remove(human.tel);
 					
+					window.getContentPane().repaint();
+					window.getContentPane().add(human);
+
+					if (human.tel != null)
+						window.getContentPane().add(human.tel);
+					result.setText(human.outputData());
 				}
 			}
 						
 		);
+		addTel.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					human.addObject();
+					human.tel.DrawWindow();
+					result.setText("added the facility is first class");
+				}
+			}
+						
+		);
+
 	}
 }	
 
